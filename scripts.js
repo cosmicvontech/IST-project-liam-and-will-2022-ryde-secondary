beb09883bc10e6f998cdd87640ed00853a721b3e
cosmicvontech <102561911+cosmicvontech@users.noreply.github.com>
2022-11-22 09:02:08 +1100

    added index.html scripts.js and styles.css
    scripts.js don't work but I still uploaded

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var shit = document.getElementById('shit')
+
+function setshit(href) {
+    shit.innerHTML = '<iframe width="450px" height="600px" id="shit" class="pageitemsandshit" href="' + href + '"></iframe>';
+};
